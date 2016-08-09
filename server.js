a35f7add1dfb040239cc7f565071236696f6106c
John Hill <john@proofpositiveimaging.com>
2016-08-09 16:34:19 -0400

    localhost fix

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 var express = require('express');
 
-//create our app
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http'){
-    nex();
-  } else {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
 app.use(express.static('public'));
 
-
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
+
+
+// var app = express();
+// var PORT = 3000;
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// });
+//
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port ' + PORT);
+// });
